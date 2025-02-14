2f8664265ed66d95c26c031a7fc9eb395dd5485f
dhenybueno <110984321+dhenybueno@users.noreply.github.com>
2025-02-14 11:44:32 -0300

    14/02

--- index.php
+++ index.php
@@ -8,10 +8,63 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    
+
 </head>
 <body>
-<h1> projeto sw2 </h1>
+    
+    <div class="container-fluid bg-primary text-white text-center p-5">
+        <h1>Projeto SW2</h1>
+        <p>
+            dhenybueno
+        </p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-sm-4"> 
+                <h1 class="bg-primary text-warning">Administrador</h1>
+                 <p class="text-primary">
+                 utilize esta opçao para realizar o cadastro
+                 de novos administradores, ou editalos
+                
+            </p>
+            </div>
+
+            <div class="col-sm-4"> 
+                <h1 class="bg-danger">Categorias</h1>
+            <p class="col-sm-4">
+                utilize esta opçao para cadastrar as Categorias
+                dos produtos que serao vendidos on-line.
+              
+           </p> 
+            </div>
+
+            <div class="col-sm-4"> 
+                <h1 class="bg-succes">Fornecedor</h1>
+                <p class="text-success">
+                utilize esta opçao para cadastrar os fornecedores
+                dos produtos on-line.
+            </p>
+            </div>
+
+        </div>
+
+    <div class="row">
+
+    <div class="col-sm-6">
+      <h1>produtos</h1>
+    </div>
+
+    <div class="col-sm-6">
+      <h1>vendas</h1>
+    </div>
+
+
+ </div>
+
+</div>
+
+
 </body>
 </html>
-
